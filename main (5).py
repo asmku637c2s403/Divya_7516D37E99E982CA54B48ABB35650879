7bffaee7888c4932da915592a2959f8db337efea
asmku637c2s403 <148335045+asmku637c2s403@users.noreply.github.com>
2023-10-18 16:53:02 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,16 @@
+def linear search product(product list, target product):
+indices = []
+for index, product in enumerate(product list):
+  if product == targetproduct:
+    indices . append (index)
+    return indices
+
+#example usuage:
+product =["shoes","boot","loafer","shoes","sandal","shoes"]
+
+target="shoes"
+target2="apple"
+result=linearsearchproduct(products,target)
+print(result)
+
+
